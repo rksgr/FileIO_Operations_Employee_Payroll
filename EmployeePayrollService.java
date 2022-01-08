4f7dc1bf6b1aa82d938a796482ba6464702a53f6
rksgr <r_kjha@yahoo.com>
2022-01-08 13:31:37 +0530

    Added welcome message

---
+++ EmployeePayrollService.java
@@ -0,0 +1,9 @@
+
+public class EmployeePayrollService {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to BridgeLabz!");
+        System.out.println("Today we shall perform basic operations related to File IO system and illustrate Employee " +
+                "Payroll System.");
+    }
+}
